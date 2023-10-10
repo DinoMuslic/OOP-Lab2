e706b137d9fe5160cd155b05300b9e3ec173d377
Dino Muslić <dino.muslic51@gmail.com>
2023-10-10 14:23:43 +0200

    adding main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Task 1
-
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a password: ");
         String pass = reader.nextLine();
