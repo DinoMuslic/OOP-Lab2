61a6acd68bf6d737208717a99b22e620b434c7bd
Dino Muslić <dino.muslic51@gmail.com>
2023-10-10 14:22:09 +0200

    task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a password: ");
+        String pass = reader.nextLine();
+
+
+        while(true) {
+            System.out.print("Type the password: ");
+            String answer = reader.nextLine();
+            if(Objects.equals(pass, answer)) {
+                System.out.println("Righ!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+
+
     }
 }
