435429a709027ed0608325e507be782d69b45056
Dino Muslić <dino.muslic51@gmail.com>
2023-10-11 15:57:27 +0200

    all tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -46,5 +47,126 @@ public class Main {
             System.out.println("Sum now: " + sum2);
         }
         System.out.println("Sum in the end: " + sum2);
+
+        // Task 4
+        System.out.print("First: ");
+        int num1 = Integer.parseInt(reader.nextLine());
+        System.out.print("Second: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+
+        if (num1 < num2) {
+            while (num1 <= num2) {
+                System.out.println(num1);
+                num1++;
+            }
+        }
+
+        // Task 5
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+
+        int sumOfNumbers = 0;
+        for (int i = 0; i <= number; i++) {
+            sumOfNumbers += (int)Math.pow(2, i);
+        }
+
+        System.out.println("The result " + sumOfNumbers);
+
+        // Task 6
+        System.out.print("How many times do you want to call the function?: ");
+        int funcNumOfTimes = Integer.parseInt(reader.nextLine());
+        for (int i = 0; i < funcNumOfTimes; i++) {
+            printText();
+        }
+
+        // Task 7
+        printStars(5);
+        printStars(3);
+        printStars(9);
+
+        // Task 8
+        drawStarPyramid(5);
+        drawStarPyramid(10);
+
+        drawStarPyramidReversed(10);
+
+        // Task 9
+        drawNumbersPyramid(5);
+        drawNumbersPyramid2(5);
+
+        // Task 10
+        int randomNum = drawNumber();
+        int numOfGuesses = 0;
+        while (true) {
+            System.out.print("Guess a number: ");
+            int guess = Integer.parseInt(reader.nextLine());
+
+            if (guess > randomNum) {
+                numOfGuesses++;
+                System.out.println("The number is lesser, guesses made: " + numOfGuesses);
+            } else if (guess < randomNum) {
+                numOfGuesses++;
+                System.out.println("The number is greater, guesses made: " + numOfGuesses);
+            } else {
+                System.out.println("Congratulations, your guess is correct");
+                break;
+            }
+        }
+
+    }
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+
+    public static void printStars(int amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void drawStarPyramid(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void drawStarPyramidReversed(int rows) {
+        for (int i = rows; rows >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            rows--;
+        }
+        System.out.println();
+    }
+
+    public static void drawNumbersPyramid(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void drawNumbersPyramid2(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int drawNumber() {
+        Random rand = new Random();
+        return rand.nextInt(100);
     }
 }
